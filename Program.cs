91c97bdb6477306355f6a62036177b4c5dae5382
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-08 17:36:26 +0500

    change console log

--- Program.cs
+++ Program.cs
@@ -6,40 +6,41 @@ namespace Keschas
 {
     class Program
     {
-        static void Main(string[]args)
+        static void Main(string[] args)
         {
- Security security = new Security();
- security.CheckPassword();
+            Security security = new Security();
+            security.CheckPassword();
 
-Bird kescha = new Bird(name:"Kescha",age:3);
-animals kasee = new Pet(name:"Kasee",age:5);
-// Polymorphism - Mavzu
+            Bird kescha = new Bird(name: "Kescha", age: 3);
+            animals kasee = new Pet(name: "Kasee", age: 5);
+            // Polymorphism - Mavzu
 
 
-Console.WriteLine("Enter your name: "); // ismingizni kiriting
- string userName = Console.ReadLine();
- System.Console.WriteLine("Keschas says:");
- kescha.Greet(userName);
- kasee.Greet(userName);
- System.Console.WriteLine("Kasee says:");
-Console.WriteLine("Enter your age"); // Yoshingizni kiriting
-string agesString = Console.ReadLine();
+            Console.WriteLine("Enter your name ex:(John): "); // ismingizni kiriting
+            string userName = Console.ReadLine();
 
-Console.WriteLine("converting..."); 
-int userAge = Convert.ToInt32(agesString);
-Console.WriteLine($"Successfully convertiSng! {userAge}");
-kescha.CalculetAgeDifferenceWithNoReturn(userAge);
-int AgeDifference  = userAge  - kescha.Age;
-//Console.WriteLine($"the difference between your and Kescha's age is: {kescha.AgeDifference} "); // sizning  va keschaning orasidagi farq
-kescha.PrintAgeDifference();
-//  teach-if
-kescha.CopeirAge(userAge);
-//Metods
+            System.Console.WriteLine("Keschas says:");
+            kescha.Greet(userName);
+            kasee.Greet(userName);
+            System.Console.WriteLine("Kasee says:");
+            Console.WriteLine("Enter your age"); // Yoshingizni kiriting
+            string agesString = Console.ReadLine();
 
-kescha.TellAboutFriends(userName,userAge);
-Console.ReadKey();
+            Console.WriteLine("converting...");
+            int userAge = Convert.ToInt32(agesString);
+            Console.WriteLine($"Successfully convertiSng! {userAge}");
+            kescha.CalculetAgeDifferenceWithNoReturn(userAge);
+            int AgeDifference = userAge - kescha.Age;
+            //Console.WriteLine($"the difference between your and Kescha's age is: {kescha.AgeDifference} "); // sizning  va keschaning orasidagi farq
+            kescha.PrintAgeDifference();
+            //  teach-if
+            kescha.CopeirAge(userAge);
+            //Metods
+
+            kescha.TellAboutFriends(userName, userAge);
+            Console.ReadKey();
         }
-    
+
     }
-} 
+}
 
